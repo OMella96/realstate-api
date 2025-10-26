51c6c4ad45b943935e31f125a89714aadb65798f
Osvaldo Mella <osv.mella96@gmail.com>
2025-10-26 15:37:46 -0300

    Cache time

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const inmuebles = [
   { id: 1, direccion: 'Calle 123', precio: 150000, tipo: 'Apartamento' },
   { id: 2, direccion: 'Avenida 456', precio: 230000, tipo: 'Casa' },
   { id: 3, direccion: 'Carrera 789', precio: 120000, tipo: 'Estudio' },
-  { id: 4, direccion: 'Nuevo 7189', precio: 120000, tipo: 'Test' }
+  { id: 4, direccion: 'Nuevo 7189', precio: 120000, tipo: 'Test3' }
 ];
 
 app.get('/api/inmuebles', (req, res) => {
