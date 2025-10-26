903be90548f489aeb9a791a12a53f80a6399e1ae
Osvaldo Mella <osv.mella96@gmail.com>
2025-10-26 15:36:34 -0300

    test nuevo recordando

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const app = express();
 const inmuebles = [
   { id: 1, direccion: 'Calle 123', precio: 150000, tipo: 'Apartamento' },
   { id: 2, direccion: 'Avenida 456', precio: 230000, tipo: 'Casa' },
-  { id: 3, direccion: 'Carrera 789', precio: 120000, tipo: 'Estudio' }
+  { id: 3, direccion: 'Carrera 789', precio: 120000, tipo: 'Estudio' },
+  { id: 4, direccion: 'Nuevo 7189', precio: 120000, tipo: 'Test' }
 ];
 
 app.get('/api/inmuebles', (req, res) => {
